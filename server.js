29487251bd4daada05725f69fe8959b82866c9e4
Tiffo <szuchinlin3@gmail.com>
2019-06-01 22:53:41 -0400

    added express logger mongoose library. coded server port and routes

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
-var cheerio = require ("cheerio");
-var axios = require ("axios");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-axios.get("https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14"). then(function(response) {
+// Models
+// var db = require("./models");
+var PORT = 3000;
 
-var $ = cheerio.load(response.data);
-var results = []
+//Express
+var app = express();
 
-$("div.productChart").each (function(i,element){
-    var title = $(element).find("a").text();
-    var link = $(element).find("a"). attr("href");
+//Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-    results.push({
-        title:title,
-        link:link
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Routes
+app.get("/wineApi", function(req, res) {
+  // list of wines
+  axios.get("https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14"). then(function(response) {
+
+    var $ = cheerio.load(response.data);
+    var results = []
+    
+    $("div.productChart").each (function(i,element){
+        var title = $(element).find("a").text();
+        var link = $(element).find("a"). attr("href");
+    
+        results.push({
+            title:title,
+            link:link
+        });
+    
     });
+    console.log(results);
+    })
 
 });
-console.log(results);
-})
+
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
